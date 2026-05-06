78ab9642a5609f3f7888d18855821942d099214d
lucianogmp <lucianolemos08@gmail.com>
2026-05-06 08:42:57 -0300

    fix: corrige base path do vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // IMPORTANTE: deve ser exatamente o nome do seu repositório no GitHub
- base: "/Gr-fica-Master-Print/",
+  base: "/Gr-fica-Master-Print/",
 });
