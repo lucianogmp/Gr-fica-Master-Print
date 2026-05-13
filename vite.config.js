0965ed93c9f448046fbba9f5f8bb7dcbacd446c7
lucianogmp <lucianolemos08@gmail.com>
2026-05-13 12:06:10 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: "/Gr-fica-Master-Print/",
-});
+  build: {
+    target: 'esnext'     // ← Essencial para suportar #
+  }
+})
