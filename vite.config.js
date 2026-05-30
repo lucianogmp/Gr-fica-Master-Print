cf0736b06210d8fd9dcaa0f64d0eb7876e6a13d9
lucianogmp <lucianolemos08@gmail.com>
2026-05-30 14:05:17 -0300

    fix: vercel config e remove vite.config.js legado

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Gr-fica-Master-Print/', // Garanta que esta linha existe e o nome está correto
-})
