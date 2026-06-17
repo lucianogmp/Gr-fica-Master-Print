341d997a6d6e0655fa4b8d76df1aba2e7d808ea9
lucianogmp <lucianolemos08@gmail.com>
2026-06-16 23:34:11 -0300

    Adiciona suporte a tema claro e escuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { AppRoutes } from './routes';
 import { Toaster } from 'react-hot-toast';
+import { useTheme } from './hooks/useTheme';
 import "./index.css";
+import "./theme.css";
 
 export default function App() {
+  useTheme();
+
   return (
     <>
       <AppRoutes />
